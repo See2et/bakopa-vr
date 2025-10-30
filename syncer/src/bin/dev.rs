35ebaf46c5530402a1356e2a97083a5aa7fe9e70
See2et / しーぜっと <61668366+See2et@users.noreply.github.com.>
2025-10-31 00:46:15 +0900

    step6

--- syncer/src/bin/dev.rs
+++ syncer/src/bin/dev.rs
@@ -1,3 +1,6 @@
-fn main() {
-    todo!()
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let args: Vec<String> = std::env::args().collect();
+    dbg!(&args);
+    todo!("write start_syncer")
 }
