b42b1160e46431523c29960c5e81c567e84c9785
See2et / しーぜっと <61668366+See2et@users.noreply.github.com.>
2025-10-31 03:23:44 +0900

    --private-key-path args

--- syncer/src/bin/dev.rs
+++ syncer/src/bin/dev.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::path::PathBuf;
 use syncer::{NodeConfig, SyncerNode};
 
 #[derive(Parser, Debug)]
@@ -31,8 +32,16 @@ async fn main() -> anyhow::Result<()> {
 
     println!("listen = {}", addr);
 
+    let key_path = if !args.private_key_path.is_empty() {
+        PathBuf::from(args.private_key_path)
+    } else {
+        PathBuf::from("")
+    };
+
+    dbg!("key_path = {:?}", &key_path);
+
     let socket_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
-    let node_config = NodeConfig::new(socket_addr);
+    let node_config = NodeConfig::new(socket_addr).with_private_key_path(key_path);
     let _ = SyncerNode::start(&node_config).await;
 
     Ok(())
