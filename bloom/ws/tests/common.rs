452989137fd0c4c519e41395405a590e97b8ae68
See2et / しーぜっと <61668366+See2et@users.noreply.github.com.>
2025-11-29 01:45:07 +0900

    RealCore

--- bloom/ws/tests/common.rs
+++ bloom/ws/tests/common.rs
@@ -6,8 +6,8 @@ use tokio::net::TcpStream;
 use tokio_tungstenite::{connect_async, MaybeTlsStream, WebSocketStream};
 use tokio_tungstenite::tungstenite::protocol::Message;
 
-pub async fn spawn_bloom_ws_server_with_core(
-    core: SharedCore<MockCore>,
+pub async fn spawn_bloom_ws_server_with_core<C: bloom_ws::CoreApi + Send + 'static>(
+    core: SharedCore<C>,
 ) -> (String, WsServerHandle) {
     let handle = start_ws_server("127.0.0.1:0".parse().unwrap(), core)
         .await
