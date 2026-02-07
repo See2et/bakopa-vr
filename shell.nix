19f41ce4b1cdd0807c7c8aa4b6c086f86f9a5a21
See2et <contact@see2et.dev>
2026-02-07 13:08:27 +0000

    chore(nix): pin rust-overlay tarball in shell.nix for reproducible eval

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 { pkgs ? import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-24.11.tar.gz") {
     overlays = [
-      (import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"))
+      (import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/470ee44393bb19887056b557ea2c03fc5230bd5a.tar.gz"))
     ];
   }
 }:
